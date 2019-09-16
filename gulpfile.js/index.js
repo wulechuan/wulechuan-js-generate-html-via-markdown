ce3be31b0daf4ecefaef59d02e1d8ae1dda2b172
wulechuan <wulechuan@live.com>
2019-09-16 14:13:57 +0800

    npm scripts

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -2,13 +2,13 @@ const highOrderTasksForBuildingPeerDependencyExamples = require(
     './tasks/build-peer-dependency-examples'
 )
 
-const cleanAll           = highOrderTasksForBuildingPeerDependencyExamples.cleanAllOldOuputs
-const buildAllThemesOnce = highOrderTasksForBuildingPeerDependencyExamples.buildEverythingOnce
-const buildAndWatch      = highOrderTasksForBuildingPeerDependencyExamples.watchEverything
+const cleanAll          = highOrderTasksForBuildingPeerDependencyExamples.cleanAllOldOuputs
+const buildExamplesOnce = highOrderTasksForBuildingPeerDependencyExamples.buildEverythingOnce
+const buildAndWatch     = highOrderTasksForBuildingPeerDependencyExamples.watchEverything
 
 module.exports ={
     cleanAll,
-    buildAllThemesOnce,
+    buildExamplesOnce,
     buildAndWatch,
 
     default: buildAndWatch,
