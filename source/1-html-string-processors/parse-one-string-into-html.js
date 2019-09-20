f50940781affdcdbe2f084b1dcde84628ff02512
wulechuan <wulechuan@live.com>
2019-09-20 15:45:55 +0800

    v1.8.3
    - identify empty string

--- source/1-html-string-processors/parse-one-string-into-html.js
+++ source/1-html-string-processors/parse-one-string-into-html.js
@@ -10,19 +10,34 @@ const STANDARD_ESPACED_CHARS_IN_STRING_LITERALS = [
 
 
 module.exports = function parseOneStringASTNodeIntoHTML(astNode) {
-    const { openMark, closeMark } = astNode
-    let { content } = astNode
+    let { content, openMark, closeMark } = astNode
 
-    STANDARD_ESPACED_CHARS_IN_STRING_LITERALS.forEach(sec => {
-        const char = sec.escapedChar
-        const coreChar = char.startsWith('\\') ? char.slice(1) : char
-        content = content.replace(
-            new RegExp(`(\\\\${char})`, 'g'),
-            `<span class="wlc-escape-char ${sec.cssClassName}"><span class="slash">\\</span><span class="escaped-char">${coreChar}</span></span>`
-        )
-    })
+    const stringIsEmpty = !!content
 
-    astNode.openMark  = `${openMark.slice(0, -1)}<span class="wlc-string-quote open-quote">${openMark.slice(-1)}</span>`
+    if (!stringIsEmpty) {
+        STANDARD_ESPACED_CHARS_IN_STRING_LITERALS.forEach(sec => {
+            const char = sec.escapedChar
+            const coreChar = char.startsWith('\\') ? char.slice(1) : char
+            content = content.replace(
+                new RegExp(`(\\\\${char})`, 'g'),
+                `<span class="wlc-escape-char ${sec.cssClassName}"><span class="slash">\\</span><span class="escaped-char">${coreChar}</span></span>`
+            )
+        })
+    }
+
+    const quoteSign = openMark.slice(-1)
+
+    if (quoteSign !== closeMark.slice(0, 1)) {
+        throw new Error('@wulechuan/hljs-plus: Different opening/closing marks of a single string.')
+    }
+
+    openMark = `<span class="hljs-string${
+        stringIsEmpty ? ' empty-string' : ''
+    }"><span class="wlc-string-quote open-quote">${quoteSign}</span>`
+
+    closeMark = `<span class="wlc-string-quote close-quote">${quoteSign}</span>${closeMark.slice(1)}`
+
+    astNode.openMark  = openMark
     astNode.content = `<span class="wlc-string-body">${content}</span>`
-    astNode.closeMark = `<span class="wlc-string-quote close-quote">${closeMark.slice(0, 1)}</span>${closeMark.slice(1)}`
+    astNode.closeMark = closeMark
 }
