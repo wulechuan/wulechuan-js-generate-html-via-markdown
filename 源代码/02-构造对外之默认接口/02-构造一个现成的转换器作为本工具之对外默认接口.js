644c56a6da4c5d56f6b2b8240daf7e3ca30e3e19
吴乐川 <wulechuan@live.com>
2021-03-13 01:37:06 +0800

    改用新版本依赖包，特别是采用了一个旧有接口的新名称。

--- 源代码/02-构造对外之默认接口/02-构造一个现成的转换器作为本工具之对外默认接口.js
+++ 源代码/02-构造对外之默认接口/02-构造一个现成的转换器作为本工具之对外默认接口.js
@@ -9,7 +9,7 @@ const 本NPM包之Peer依赖包 = require(本NPM包之Peer依赖包之名称)
 const 构建一个用于将Markdown内容字符串转换为HTML字符串的转换器 = require('../01-转换器之构建器')
 
 const 将Markdown内容字符串转换为HTML内容的转换器 = 构建一个用于将Markdown内容字符串转换为HTML字符串的转换器({
-    themesPeerPackageAllDistFileEntriesKeyingByFileNames: 本NPM包之Peer依赖包.以文件名称为索引之所有文件之字典,
+    themesPeerPackageAllDistFileEntriesKeyingByFileNames: 本NPM包之Peer依赖包.以文件名称为索引之所有文件简易描述项之字典,
     syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 本NPM包之Peer依赖包.获取某一已发布之文件之完整内容字符串,
     不应采纳本工具之源代码之缓存版本以应对本工具研发阶段之要求: false,
 })
