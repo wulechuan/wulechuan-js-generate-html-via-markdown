a47e0b78223691fe5f2da2d8e7b5258443b3c77f
吴乐川 <wulechuan@live.com>
2021-03-14 02:59:12 +0800

    升级了依赖包故而得到了正确的类型定义。

--- 测试集/测试-typescript-定义之有效性.js
+++ 测试集/测试-typescript-定义之有效性.js
@@ -1,18 +1,75 @@
-import 一枚现成的转换器 from '..'
-
 /**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
  * 我不打算令 nodejs 运行本 JavaScript 文件之代码。
  * 设计本文件纯粹是为了在其中测试本工具之 TypeScript 定义是否起效。
  * 具体而言，我会在本文中打字，观察我的代码编辑器（ VSCode ）能否给出正确的代码提示。
+ *
+ *
+ *
+ * 2021-03-13 实验结果：一切良好。
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
-一枚现成的转换器.构建一个用于将Markdown内容字符串转换为HTML字符串的转换器({
+
+
+
+
+/** */
+import 一枚现成的转换器 from '..'
+
+import {
+    获取某一已发布之文件之完整内容字符串,
+    以文件名称为索引之所有文件之字典,
+} from '@wulechuan/css-stylus-markdown-themes'
+
+
+
+
+
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 请输入以下测试性文字，并观察代码编辑器是否能给出正确的代码提示。
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * const 一枚新的转换器 = 一枚现成的转换器.构建一个用于将Markdown内容字符串转换为HTML字符串的转换器({
+ *     不应采纳本工具之源代码之缓存版本以应对本工具研发阶段之要求: true,
+ *     syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 获取某一已发布之文件之完整内容字符串,
+ *     themesPeerPackageAllDistFileEntriesKeyingByFileNames: 以文件名称为索引之所有文件之字典,
+ * })
+ * 一枚新的转换器('# 自我简介\n\n无。', {
+ *     对HTML做进一步处理之阶段: {
+ *         不应将代码块中的换行符替换成BR标签: true,
+ *     },
+ * })
+ */
+/** */
+const 一枚新的转换器 = 一枚现成的转换器.构建一个用于将Markdown内容字符串转换为HTML字符串的转换器({
     不应采纳本工具之源代码之缓存版本以应对本工具研发阶段之要求: true,
-    themesPeerPackageAllDistFileEntriesKeyingByFileNames: {
-        'a': {
-            
-        }
-    }
+    syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 获取某一已发布之文件之完整内容字符串,
+    themesPeerPackageAllDistFileEntriesKeyingByFileNames: 以文件名称为索引之所有文件之字典,
+})
+一枚新的转换器('# 自我简介\n\n无。', {
+    对HTML做进一步处理之阶段: {
+        不应将代码块中的换行符替换成BR标签: true,
+    },
 })
 
-/** 2021-03-13 实验结果：一切良好。 */
+
+
+
+
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 请输入以下测试性文字，并观察代码编辑器是否能给出正确的代码提示。
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * const defaultOptions = 一枚现成的转换器.完备的默认配置集
+ * const html = 一枚现成的转换器('# 一篇好文\n\n中国加油！', defaultOptions)
+ * const charCounts = html.length
+ */
+/** */
+const defaultOptions = 一枚现成的转换器.完备的默认配置集
+const html = 一枚现成的转换器('# 一篇好文\n\n中国加油！', defaultOptions)
+const charCounts = html.length
--- 源代码/types/index.d.ts
+++ 源代码/types/index.d.ts
@@ -1,75 +1,66 @@
-/// <reference types="@wulechuan/css-stylus-markdown-themes" />
+import type {
+    类型定义之由本工具内建之层叠样式表文件之名称 as 依赖包之类型定义之内建之层叠样式表文件之名称,
+    类型定义之由本工具内建之层叠样式表文件之名称 as 依赖包之类型定义之内建之层叠样式表文件之名称之凡不含文章目录者,
+    类型定义之由本工具内建之层叠样式表文件之名称 as 依赖包之类型定义之内建之层叠样式表文件之名称之凡含有文章目录者,
+    // 类型定义之内建之层叠样式表文件之名称 as 依赖包之类型定义之内建之层叠样式表文件之名称,
+    // 类型定义之内建之层叠样式表文件之名称之凡不含文章目录者 as 依赖包之类型定义之内建之层叠样式表文件之名称之凡不含文章目录者,
+    // 类型定义之内建之层叠样式表文件之名称之凡含有文章目录者 as 依赖包之类型定义之内建之层叠样式表文件之名称之凡含有文章目录者,
+    类型定义之文件简易描述项字典 as 依赖包之类型定义之文件简易描述项字典,
+    类型定义之获取某特定文件之完整内容字符串之函数 as 依赖包之类型定义之获取某特定文件之完整内容字符串之函数,
+} from '@wulechuan/css-stylus-markdown-themes';
+
+
+
+
 
 declare function 一枚现成的自Markdown至HTML的转换器 (
-    markdown内容全文: string,
-    配置项集?: 一枚现成的自Markdown至HTML的转换器.将Markdown字符串转换为HTML字符串之转换器之配置项集
-): string;
+    ...args: Parameters<一枚现成的自Markdown至HTML的转换器.自Markdown至HTML的转换器>
+): ReturnType<一枚现成的自Markdown至HTML的转换器.自Markdown至HTML的转换器>;
 
-export default 一枚现成的自Markdown至HTML的转换器;
+export = 一枚现成的自Markdown至HTML的转换器;
+
+
+
+
 
 declare namespace 一枚现成的自Markdown至HTML的转换器 {
-    export const 构建一个用于将Markdown内容字符串转换为HTML字符串的转换器: 类型之转换器之构建器;
+    export const 构建一个用于将Markdown内容字符串转换为HTML字符串的转换器: 类型定义之转换器之构建器;
     export const 完备的默认配置集: 将Markdown字符串转换为HTML字符串之转换器之配置项集;
 
 
 
 
 
-    // import 本NPM包之Peer依赖包 from '@wulechuan/css-stylus-markdown-themes';
+    export type 类型定义之转换器之构建器 = (配置项集: 类型定义之转换器之构建器之配置项集) => 自Markdown至HTML的转换器;
 
-
-
-
-
-    export type 类型之转换器之构建器 = (配置项集: 类型之转换器之构建器之配置项集) => 自Markdown至HTML的转换器;
-
-    export type 类型之转换器之构建器之配置项集 = {
-        // themesPeerPackageAllDistFileEntriesKeyingByFileNames: 吴乐川用于美化文章的层叠样式表集之数据类型之文件简易描述项字典;
-        // syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 吴乐川用于美化文章的层叠样式表集之数据类型之获取某特定文件之完整内容字符串之函数;
+    export type 类型定义之转换器之构建器之配置项集 = {
+        themesPeerPackageAllDistFileEntriesKeyingByFileNames: 依赖包之类型定义之文件简易描述项字典;
+        syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 依赖包之类型定义之获取某特定文件之完整内容字符串之函数;
         不应采纳本工具之源代码之缓存版本以应对本工具研发阶段之要求?: boolean;
     };
+
+    /**
+     * 不应采用 “ typeof 一枚现成的自Markdown至HTML的转换器 ” 之写法。
+     * 因为这一定义还额外包含了
+     *     1） 构建一个用于将Markdown内容字符串转换为HTML字符串的转换器
+     *     2） 完备的默认配置集
+     * 这两个属性。故而是不正确的。
+     *
+     * 恰恰相反，应对令上文 “ 一枚现成的自Markdown至HTML的转换器 ” 函数之定义尽可能采用下方之现成定义。此举已成。
+     */
+    // export type 自Markdown至HTML的转换器 = typeof 一枚现成的自Markdown至HTML的转换器;
 
     export type 自Markdown至HTML的转换器 = (
         markdown内容全文: string,
         配置项集?: 一枚现成的自Markdown至HTML的转换器.将Markdown字符串转换为HTML字符串之转换器之配置项集
     ) => string;
 
-    export type 类型之由本工具内建之层叠样式表文件之名称 = (
-        | 'wulechuan-styles-for-html-via-markdown--firefox-addon.default.css'
-        | 'wulechuan-styles-for-html-via-markdown--typora.default.css'
-        | 'wulechuan-styles-for-html-via-markdown--vscode.default.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--no-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--no-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--with-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--with-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--wrapped--no-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--wrapped--no-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--wrapped--with-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default--wrapped--with-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--no-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--no-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--with-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--with-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--wrapped--no-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--wrapped--no-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--wrapped--with-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.default-dark--wrapped--with-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--no-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--no-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--with-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--with-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--wrapped--no-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--wrapped--no-toc.min.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--wrapped--with-toc.css'
-        | 'wulechuan-styles-for-html-via-markdown.generic-default-light--atom-one-dark--wrapped--with-toc.min.css'
-    );
-
-    export type 类型之作用于HTML内容之内建替换规则名称 = (
+    export type 类型定义之作用于HTML内容之内建替换规则名称 = (
         | '令所有外部链接之打开方式为 _blank'
         | '令所有原本指向 Markdown 文件之链接改为指向同名 HTML 文件'
     );
 
-    export type 类型之作用于HTML内容之额外替换规则项 = {
+    export type 类型定义之作用于HTML内容之额外替换规则项 = {
         凡: string | RegExp;
         替换为: string;
     };
@@ -113,8 +104,8 @@ declare namespace 一枚现成的自Markdown至HTML的转换器 {
 
             产出之HTML文件之Title标签之内容字符串?: string;
 
-            所采用之由本工具内建之不含文章纲要列表之定义之层叠样式表文件之名称?: 一枚现成的自Markdown至HTML的转换器.类型之由本工具内建之层叠样式表文件之名称;
-            所采用之由本工具内建之含有文章纲要列表之定义之层叠样式表文件之名称?: 一枚现成的自Markdown至HTML的转换器.类型之由本工具内建之层叠样式表文件之名称;
+            所采用之由本工具内建之不含文章纲要列表之定义之层叠样式表文件之名称?: 依赖包之类型定义之内建之层叠样式表文件之名称之凡不含文章目录者;
+            所采用之由本工具内建之含有文章纲要列表之定义之层叠样式表文件之名称?: 依赖包之类型定义之内建之层叠样式表文件之名称之凡含有文章目录者;
 
             本工具专门可配置的层叠样式表类名集?: {
                 __cssClassNameToRename__MarkdownChiefContentWrappingArticleTag?: string;
@@ -123,8 +114,8 @@ declare namespace 一枚现成的自Markdown至HTML的转换器 {
             };
 
             须对产出之HTML内容字符串依次按下诸内容替换规则做修订?: {
-                '1 内建现成的替换规则序列'?: Array<一枚现成的自Markdown至HTML的转换器.类型之作用于HTML内容之内建替换规则名称>;
-                '2 额外的替换规则序列'?: Array<一枚现成的自Markdown至HTML的转换器.类型之作用于HTML内容之额外替换规则项>;
+                '1 内建现成的替换规则序列'?: Array<一枚现成的自Markdown至HTML的转换器.类型定义之作用于HTML内容之内建替换规则名称>;
+                '2 额外的替换规则序列'?: Array<一枚现成的自Markdown至HTML的转换器.类型定义之作用于HTML内容之额外替换规则项>;
             };
 
             凡内容须注入产出之HTML中之所有外来文件?: {
@@ -135,7 +126,6 @@ declare namespace 一枚现成的自Markdown至HTML的转换器 {
 
         对本工具现成提供的文章纲要做以下配置?: {
             为求文章纲要列表简洁明了故意仅显示两层条目以至于较深层级条目形同作废?: boolean;
-
             浏览器打开HTML文章最初之时文章纲要列表中凡层级深于该值之条目均应收叠?: number;
             浏览器打开HTML文章最初之时若浏览器窗口足够宽大则直接展开文章纲要列表之面板?: boolean;
         };
