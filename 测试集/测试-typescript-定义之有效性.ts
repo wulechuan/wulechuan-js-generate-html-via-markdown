85e95f2b9ba7baed2d11bee864c26d0958553ac6
吴乐川 <wulechuan@live.com>
2021-03-14 03:17:21 +0800

    从依赖包舶来的类型定义终于圆满了。

--- 测试集/测试-typescript-定义之有效性.ts
+++ 测试集/测试-typescript-定义之有效性.ts
@@ -21,7 +21,7 @@ import 一枚现成的转换器 from '..'
 
 import {
     获取某一已发布之文件之完整内容字符串,
-    以文件名称为索引之所有文件之字典,
+    以文件名称为索引之所有文件简易描述项之字典,
 } from '@wulechuan/css-stylus-markdown-themes'
 
 
@@ -36,7 +36,7 @@ import {
  * const 一枚新的转换器 = 一枚现成的转换器.构建一个用于将Markdown内容字符串转换为HTML字符串的转换器({
  *     不应采纳本工具之源代码之缓存版本以应对本工具研发阶段之要求: true,
  *     syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 获取某一已发布之文件之完整内容字符串,
- *     themesPeerPackageAllDistFileEntriesKeyingByFileNames: 以文件名称为索引之所有文件之字典,
+ *     themesPeerPackageAllDistFileEntriesKeyingByFileNames: 以文件名称为索引之所有文件简易描述项之字典,
  * })
  * 一枚新的转换器('# 自我简介\n\n无。', {
  *     对HTML做进一步处理之阶段: {
@@ -48,7 +48,7 @@ import {
 const 一枚新的转换器 = 一枚现成的转换器.构建一个用于将Markdown内容字符串转换为HTML字符串的转换器({
     不应采纳本工具之源代码之缓存版本以应对本工具研发阶段之要求: true,
     syncGetContentStringOfOneFileOfThePeerModuleOfThemes: 获取某一已发布之文件之完整内容字符串,
-    themesPeerPackageAllDistFileEntriesKeyingByFileNames: 以文件名称为索引之所有文件之字典,
+    themesPeerPackageAllDistFileEntriesKeyingByFileNames: 以文件名称为索引之所有文件简易描述项之字典,
 })
 一枚新的转换器('# 自我简介\n\n无。', {
     对HTML做进一步处理之阶段: {
@@ -73,3 +73,17 @@ const 一枚新的转换器 = 一枚现成的转换器.构建一个用于将Mark
 const defaultOptions = 一枚现成的转换器.完备的默认配置集
 const html = 一枚现成的转换器('# 一篇好文\n\n中国加油！', defaultOptions)
 const charCounts = html.length
+
+
+
+
+
+/**
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * 请输入以下测试性文字，并观察代码编辑器是否能给出正确的代码提示。
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * console.log(以文件名称为索引之所有文件简易描述项之字典['wulechuan-styles-for-html-via-markdown--firefox-addon.default.css'].文件之相对路径)
+ */
+/** */
+console.log(以文件名称为索引之所有文件简易描述项之字典['wulechuan-styles-for-html-via-markdown--firefox-addon.default.css'].文件之相对路径)
