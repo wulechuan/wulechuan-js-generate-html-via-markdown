42352dd5dcf0f007929ce1c66777134a50d4921b
吴乐川 <wulechuan@live.com>
2022-04-29 16:12:27 +0800

    命令行脚本改进。

--- 用于研发阶段的命令行工具/bash/Update-更新所有npm依赖包至最新版本.sh
+++ 用于研发阶段的命令行工具/bash/Update-更新所有npm依赖包至最新版本.sh
@@ -5,12 +5,22 @@
 # ------------------------------------------------------
 
 # 以下均为须采用特定版本的【产品依赖包】。
+echo
+echo
+echo
+echo  -e  "\e[0;31m===== npm i    \e[97;41m特定版本\e[0;31m的【产品依赖包】 ============================\e[0m"
+echo
 npm  i \
     'chalk@4' # 不能更新至第 5 过更晚的版本。因为自第 5 版始， chalk 仅支持 ES Module 语法。
 
 
 
 # 以下均为可采用 latest 版本的【产品依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+echo
+echo
+echo
+echo  -e  "\e[0;31m===== npm i    特定版本的【产品依赖包】 ============================\e[0m"
+echo
 npm i \
     '@wulechuan/meaningful-error-messages@latest' \
     'fs-extra@latest' \
@@ -28,11 +38,21 @@ npm i \
 # ---------- 【研发依赖包】 ------------------------------
 # ------------------------------------------------------
 # 以下均为须采用特定版本的【研发依赖包】。
+# echo
+# echo
+# echo
+# echo  -e  "\e[32m===== npm i    \e[90;102m特定版本\e[0;32m的【研发依赖包】 ============================\e[0m"
+# echo
 # 暂无。
 
 
 
 # 以下均为可采用 latest 版本的【研发依赖包】。顺便提醒，虽然一般而言 latest 版本应恰为最高版本，但并不确保。
+echo
+echo
+echo
+echo  -e  "\e[32m===== npm i    特定版本的【研发依赖包】 ============================\e[0m"
+echo
 npm i -D \
     '@wulechuan/cli-scripts--git-push@latest' \
     '@wulechuan/css-stylus-markdown-themes@latest' \
